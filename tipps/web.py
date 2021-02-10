b0f57ebfb8135bf1e1d47a0d76b35c001a54fef7
Jonas Neugebauer <jonas@neugebauer.cc>
2021-02-10 16:24:53 +0100

    Added a landing page

--- tipps/web.py
+++ tipps/web.py
@@ -7,30 +7,39 @@ from tipps.util import *
 
 web = Blueprint('web', __name__)
 
-@web.route('/')
+@web.route('/', methods=['GET', 'POST'])
 def start():
-	return render_template('input.html', templates=get_templates())
+	if request.method == 'POST':
+		code = request.form.get('code', default=None, type=str)
+		if code:
+			return redirect(url_for('web.show_tipp', id=id), 303)
+		return render_template('index.html', error='Zu deinem Code konnten wir leider keinen Tipp finden. Hast du dich vielleicht vertippt? Versuch es ruhig noch einmal.')
+	else:
+		return render_template('index.html')
 
-@web.route('/create', methods=['POST'])
+@web.route('/create', methods=['GET', 'POST'])
 def create():
-	content = request.form.get('content', default='', type=str)
-	token = request.form.get('token', default='', type=str)
-	template = request.form.get('template', default='default', type=str)
+	if request.method == 'POST':
+		content = request.form.get('content', default='', type=str)
+		token = request.form.get('token', default='', type=str)
+		template = request.form.get('template', default='default', type=str)
 
-	if len(content.strip()) == 0 or len(token.strip()) == 0:
-		return render_template('input.html', content=content, token=token, template=template, templates=get_templates(), error="Der Inhalt darf nicht leer sein.")
+		if len(content.strip()) == 0 or len(token.strip()) == 0:
+			return render_template('input.html', content=content, token=token, template=template, templates=get_templates(), error="Der Inhalt darf nicht leer sein.")
 
-	db = get_db()
-	user_id = db.execute('SELECT id FROM user WHERE token = ?', (token,)).fetchone()
-	if not user_id:
-		return render_template('input.html', content=content, token=token, template=template, templates=get_templates(), error="Kein gültiges Token.")
+		db = get_db()
+		user_id = db.execute('SELECT id FROM user WHERE token = ?', (token,)).fetchone()
+		if not user_id:
+			return render_template('input.html', content=content, token=token, template=template, templates=get_templates(), error="Kein gültiges Token.")
 
-	id = create_tipp(content, template=template)
+		id = create_tipp(content, template=template)
 
-	db.execute('INSERT INTO tipp (id,user_id,template) VALUES (?, ?, ?)', (id, user_id['id'], template,))
-	db.commit()
+		db.execute('INSERT INTO tipp (id,user_id,template) VALUES (?, ?, ?)', (id, user_id['id'], template,))
+		db.commit()
 
-	return redirect(url_for('web.show_tipp', id=id), 303)
+		return redirect(url_for('web.show_tipp', id=id), 303)
+	else:
+		return render_template('input.html', templates=get_templates())
 
 
 @web.route('/<string:id>')
